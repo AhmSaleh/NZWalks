430df405e1a1d2ce621b9a4022d40e80e39f2eed
ahmsaleh <ahmedsaleh166@gmail.com>
2022-08-19 17:40:17 +0200

    End of section 7

---
+++ NZWalks/NZWalksAPI/Validators/UpdateRegionRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace NZWalksAPI.Validators
+{
+    public class UpdateRegionRequestValidator : AbstractValidator<Models.DTOs.AddRegionRequest>
+    {
+        public UpdateRegionRequestValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Area).GreaterThan(0);
+            RuleFor(x => x.Population).GreaterThanOrEqualTo(0);
+        }
+    }
+}
